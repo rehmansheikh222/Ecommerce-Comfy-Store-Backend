75730479e95712e741a13fe219c1db6720b07d28
unknown <sh.rehman222@gmail.com>
2023-03-08 17:48:20 +0500

    Express Setup

--- app.js
+++ app.js
@@ -1,1 +1,16 @@
-console.log('E-Commerce API');
+// Express
+const express = require('express')
+const app = express()
+const port = process.env.PORT || 5000
+
+const start = async (req, res) => {
+  try {
+    app.listen(port, () => {
+      console.log(`Server is running on port ${port}`)
+    })
+  } catch (error) {
+    console.log(error)
+  }
+}
+
+start()
