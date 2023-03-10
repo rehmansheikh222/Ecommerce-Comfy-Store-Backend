0ccae3d8b6b58040e15f40ee6d2b31f056c5672e
unknown <sh.rehman222@gmail.com>
2023-03-10 19:04:24 +0500

    Cookies Setup

--- controllers/authController.js
+++ controllers/authController.js
@@ -19,8 +19,13 @@ const register = async (req, res) => {
 
   const tokenUser = { name: user.name, userId: user._id, role: user.role }
   const token = createJWT({ payload: tokenUser })
+  const oneDay = 100 * 60 * 60 * 24
+  res.cookie('token', token, {
+    httpOnly: true,
+    expires: new Date(Date.now() + oneDay),
+  })
 
-  res.status(StatusCodes.CREATED).json({ user: tokenUser, token })
+  res.status(StatusCodes.CREATED).json({ user: tokenUser })
 }
 const login = async (req, res) => {
   res.send('Login user')
