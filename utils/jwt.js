d0fccad7e75e54f7bbf8d51a3b5707798a874be9
unknown <sh.rehman222@gmail.com>
2023-03-14 22:23:31 +0500

    Refactor Cookies

--- utils/jwt.js
+++ utils/jwt.js
@@ -1,5 +1,5 @@
 const jwt = require('jsonwebtoken')
-
+const { StatusCodes } = require('http-status-codes')
 const createJWT = ({ payload }) => {
   const token = jwt.sign(payload, process.env.JWT_SECRET, {
     expiresIn: process.env.JWT_LIFETIME,
@@ -9,7 +9,18 @@ const createJWT = ({ payload }) => {
 
 const isTokenValid = ({ token }) => jwt.verify(token, process.env.JWT_SECRET)
 
+const attachCookiesToResponse = ({ res, user }) => {
+  const token = createJWT({ payload: user })
+  const oneDay = 100 * 60 * 60 * 24
+  res.cookie('token', token, {
+    httpOnly: true,
+    expires: new Date(Date.now() + oneDay),
+  })
+  // res.status(StatusCodes.CREATED).json({ user: user })
+}
+
 module.exports = {
   createJWT,
   isTokenValid,
+  attachCookiesToResponse,
 }
