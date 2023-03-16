180ed0fedb56b750fc90553980289ccc3e3ca8e7
unknown <sh.rehman222@gmail.com>
2023-03-16 22:15:04 +0500

    Authenticate user complete

--- middleware/authentication.js
+++ middleware/authentication.js
@@ -1,15 +1,19 @@
 const CustomError = require('../errors')
 
-const { isTokenvalid } = require('../utils')
+const { isTokenValid } = require('../utils')
 
 const authenticateUser = async (req, res, next) => {
   const token = req.signedCookies.token
   if (!token) {
-    console.log('error, no token present')
-  } else {
-    console.log('token present')
+    throw new CustomError.UnauthenticatedError('Authentication Invalid')
   }
-  next()
+  try {
+    const { name, userId, role } = isTokenValid({ token })
+    req.user = { name, userId, role }
+    next()
+  } catch (error) {
+    throw new CustomError.UnauthenticatedError('Authentication Invalid')
+  }
 }
 
 module.exports = {
