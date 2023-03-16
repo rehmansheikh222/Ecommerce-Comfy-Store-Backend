6033b13d1f7865957b8c0839ab55f2e12c056010
unknown <sh.rehman222@gmail.com>
2023-03-16 16:01:50 +0500

    getallusers and getsingleuser routes

--- controllers/userController.js
+++ controllers/userController.js
@@ -1,8 +1,16 @@
+const User = require('../models/User')
+const { StatusCodes } = require('http-status-codes')
+const CustomError = require('../errors')
 const getAllUsers = async (req, res) => {
-  res.send('Get All users Route')
+  const users = await User.find({ role: 'user' }).select('-password')
+  res.status(StatusCodes.OK).json({ users })
 }
 const getSingleUser = async (req, res) => {
-  res.send('Get Single user Route')
+  const user = await User.findOne({ _id: req.params.id }).select('-password')
+  if (!user) {
+    throw new CustomError.NotFoundError(`No user with id ${req.params.id}`)
+  }
+  res.status(StatusCodes.OK).json({ user })
 }
 const showCurrentUser = async (req, res) => {
   res.send('Show Current User Route')
